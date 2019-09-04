b602ddba83aca8acbab18cd184d4f2ffcdb128a0
kerianne16 <kerianne116@gmail.com>
2019-09-03 21:40:12 -0400

    initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
+import Login from './Login';
+import '../style.css';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+                    <h3>Book with Bombshell Beauty</h3>
+                    <NavLink to="/login">Login/Sign Up</NavLink>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Main;
