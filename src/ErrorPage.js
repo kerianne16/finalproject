fd1c567ca93d7d7ee22f7e954d8990b30c9ac87b
kerianne16 <kerianne116@gmail.com>
2019-09-05 15:10:16 -0400

    details

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Login from './Login';
+
 class ErrorPage extends React.Component {
   render() {
     return (
       <div className="container">
         <h4>Follow Us on Instagram</h4>
-        <Login />
+
       </div>
     );
   }
